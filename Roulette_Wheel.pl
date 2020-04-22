41097b1de7352a31358598a8e2af5f6d7fd690a7
Eric <63894482+WarAngel-Eric@users.noreply.github.com>
2020-04-21 22:10:04 -0700

    Delete Roulette_Wheel.pl

--- Roulette_Wheel.pl
+++
@@ -1,9 +0,0 @@
-#NPCID 2596 Roulette_Wheel in poknowledge v.0
-#Set to type 11 (untargetable) after placing in database
-
-sub EVENT_SIGNAL {
-	if ($signal == 1) {
-		$npc->CastSpell(5106,$npc);
-	}
-}
-
